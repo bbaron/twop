eea5e8313a5a2b424a0c20911ff153ea890a7f26
Bruce Baron <baron.br@gmail.com>
2020-04-05 11:45:56 -0400

    spring boot starter

---
+++ src/main/java/twop/TwopApplication.java
@@ -0,0 +1,13 @@
+package twop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwopApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TwopApplication.class, args);
+  }
+
+}
---
+++ src/test/java/twop/TwopApplicationTests.java
@@ -0,0 +1,13 @@
+package twop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwopApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
